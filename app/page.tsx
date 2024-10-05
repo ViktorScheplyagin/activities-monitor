d83c7845f57ef96fd5b22853d74b79882ad17288
Viktor Schepliahin <silverman995@gmail.com>
2024-10-05 17:16:47 +0200

    add milestones for the nearest future

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
-import HomePage from "@/pages/Home";
+import { HomePage } from "@/pages/Home";
 
 export default function Home() {
   return <HomePage />;
 }
 
-// TODO: Add index.ts files to components and utils folders
-// TODO: Add a notification sound or OS pop up when the timer is done
+// TODO: review the folder structure and naming
+// TODO: implement the conventional commits pattern
+// TODO: move all the TODOs to the project board
+// TODO: cover with tests
+
+// TODO: add a tasks list
+// TODO: add a highlight of an active task
+// TODO: add checkboxes to tasks
+// TODO: add a status to tasks
+// TODO: show how much time was spent on a task
+// TODO: add "delete" and "edit" buttons to tasks
+// TODO: add drag and drop functionality to tasks
