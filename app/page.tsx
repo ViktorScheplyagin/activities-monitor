44396509e9c0a8ec44af1f466221e3918887251b
Viktor Schepliahin <silverman995@gmail.com>
2024-11-08 22:03:51 +0100

    feat: add a Prisma client, schema and migrate

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,4 @@ export default function Home() {
   return <HomePage />;
 }
 
-// TODO: review the folder structure and naming
 // TODO: implement the conventional commits pattern
-// TODO: move all the TODOs to the project board
-
-// TODO: cover with tests
-// TODO: deploy to Vercel
-// TODO: add CI/CD
-
-// TODO: add a tasks list
-// TODO: add a highlight of an active task
-// TODO: add checkboxes to tasks
-// TODO: add a status to tasks
-// TODO: show how much time was spent on a task
-// TODO: add "delete" and "edit" buttons to tasks
-// TODO: add drag and drop functionality to tasks
