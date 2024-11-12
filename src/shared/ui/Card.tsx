ae5851d0c36e8bb0f17aafed0c7deda37ddf5630
Viktor Schepliahin <silverman995@gmail.com>
2024-11-12 20:14:55 +0100

    fix: naturalize the Card component props

--- src/shared/ui/Card.tsx
+++ src/shared/ui/Card.tsx
@@ -1,7 +1,10 @@
-"use client";
-export const Card = ({ children }: { children: React.ReactNode }) => {
+type Props = React.PropsWithChildren<React.HTMLAttributes<HTMLDivElement>>;
+
+export const Card = ({ children, ...props }: Props) => {
   return (
-    <div className="bg-white p-8 rounded-lg shadow-md dark:bg-gray-800">
+    <div
+      className={`bg-white p-8 rounded-lg shadow-md dark:bg-gray-800 ${props.className}`}
+    >
       {children}
     </div>
   );
