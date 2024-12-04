e332e91dfba4358440919d108e211174c0ae8022
Viktor Schepliahin <silverman995@gmail.com>
2024-12-04 17:34:09 +0100

    feat: add new SVG icons (CheckIcon, InboxIcon, PauseIcon, PlayIcon, StopIcon) and update icons index export

---
+++ src/shared/lib/icons/index.ts
@@ -0,0 +1,5 @@
+export { PlayIcon } from "./PlayIcon";
+export { StopIcon } from "./StopIcon";
+export { PauseIcon } from "./PauseIcon";
+export { CheckIcon } from "./CheckIcon";
+export { InboxIcon } from "./InboxIcon";
