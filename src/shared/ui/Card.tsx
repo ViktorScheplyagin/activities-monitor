0438a80d8595e45d78c90eb16a1d6e2a6cbe9977
Viktor Schepliahin <silverman995@gmail.com>
2024-12-04 17:36:01 +0100

    fix: pass props to Card component for improved flexibility in rendering; ensure additional attributes are correctly applied

--- src/shared/ui/Card.tsx
+++ src/shared/ui/Card.tsx
@@ -3,6 +3,7 @@ type Props = React.PropsWithChildren<React.HTMLAttributes<HTMLDivElement>>;
 export const Card = ({ children, ...props }: Props) => {
   return (
     <div
+      {...props}
       className={`bg-white p-8 rounded-lg shadow-md dark:bg-gray-800 ${props.className}`}
     >
       {children}
