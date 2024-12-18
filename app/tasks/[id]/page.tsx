d1895e057cab1ecfd3fc6a5865d0396bdb4e7ff9
Viktor Schepliahin <silverman995@gmail.com>
2024-12-18 21:12:27 +0100

    refactor: migrate pages to pages-layer structure; update imports in Home and Task pages

--- app/tasks/[id]/page.tsx
+++ app/tasks/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { TaskPage as TaskPageComponent } from "@/pages/task";
+import { TaskPage as TaskPageComponent } from "@/pages-layer/task";
 
 export default function TaskPage({ params }: { params: { id: string } }) {
   return <TaskPageComponent id={params.id} />;
