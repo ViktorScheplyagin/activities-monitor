b811a82f9eda8638b46dc0a75a26d3e93315bf36
Viktor Schepliahin <silverman995@gmail.com>
2025-01-04 17:10:56 +0100

    feat(migrate to neumorpism): use neumorphic components task-search bar

--- src/features/task-search/ui/TaskSearch.tsx
+++ src/features/task-search/ui/TaskSearch.tsx
@@ -1,36 +1,40 @@
-import { Checkbox, Input, Label } from "@/shared/ui";
+import { Label } from "@/shared/ui";
+import { Input, Checkbox } from "@/shared/ui/neomorphic";
 import { useTaskSearch } from "../model/use-task-search";
 
 const TaskSearch = () => {
-  const { query, filters, setQuery, toggleFilter } = useTaskSearch();
+    const { query, filters, setQuery, toggleFilter } = useTaskSearch();
 
-  return (
-    <div className="space-y-4">
-      <Input
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        placeholder="Search tasks..."
-      />
-      <div className="flex gap-4">
-        <Label className="flex items-center gap-2" htmlFor="name">
-          <Checkbox
-            id="name"
-            checked={filters.includes("name")}
-            onClick={() => toggleFilter("name")}
-          />
-          Search in names
-        </Label>
-        <Label className="flex items-center gap-2" htmlFor="description">
-          <Checkbox
-            id="description"
-            checked={filters.includes("description")}
-            onClick={() => toggleFilter("description")}
-          />
-          Search in descriptions
-        </Label>
-      </div>
-    </div>
-  );
+    return (
+        <div className="space-y-4">
+            <Input
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                placeholder="Search tasks..."
+            />
+            <div className="flex gap-4">
+                <Label className="flex items-center gap-2" htmlFor="name">
+                    <Checkbox
+                        id="name"
+                        checked={filters.includes("name")}
+                        onClick={() => toggleFilter("name")}
+                    />
+                    Search in names
+                </Label>
+                <Label
+                    className="flex items-center gap-2"
+                    htmlFor="description"
+                >
+                    <Checkbox
+                        id="description"
+                        checked={filters.includes("description")}
+                        onClick={() => toggleFilter("description")}
+                    />
+                    Search in descriptions
+                </Label>
+            </div>
+        </div>
+    );
 };
 
 export default TaskSearch;
