49e56c97cb5f2face9bf6e7bb2391fed7f526e43
Viktor Schepliahin <silverman995@gmail.com>
2025-01-04 19:20:57 +0100

    feat(migrate to neumorpism): Task, CreateTaskDialog, DeleteTaskDialog components

--- src/entities/task/ui/Task.tsx
+++ src/entities/task/ui/Task.tsx
@@ -1,42 +1,45 @@
-import { Card } from "@/shared/ui";
+import { Card } from "@/shared/ui/neomorphic";
 import { TaskData } from "../api/dto/task";
 import { Trash2 } from "lucide-react";
 import { formatSeconds } from "../lib/formatSeconds";
 
 interface Props {
-  task: TaskData;
-  className?: string;
-  onDeleteClick?: (taskId: string) => void;
+    task: TaskData;
+    className?: string;
+    onDeleteClick?: (taskId: string) => void;
 }
 
 export const Task = ({ task, className, onDeleteClick }: Props) => {
-  const handleDeleteClick = (e: React.MouseEvent) => {
-    e.preventDefault(); // Prevent link navigation
-    onDeleteClick?.(task.id);
-  };
+    const handleDeleteClick = (e: React.MouseEvent) => {
+        e.preventDefault(); // Prevent link navigation
+        onDeleteClick?.(task.id);
+    };
 
-  return (
-    <Card
-      className={`cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-800 transition-colors ${className}`}
-    >
-      <div className="p-4 flex justify-between items-start">
-        <div>
-          <h3 className="text-lg font-medium">{task.title}</h3>
-          <p className="text-gray-500 dark:text-gray-400">{task.description}</p>
-        </div>
-        {onDeleteClick && (
-          <button
-            onClick={handleDeleteClick}
-            className="p-2 text-gray-500 hover:text-red-500 transition-colors"
-            aria-label="Delete task"
-          >
-            <Trash2 className="h-5 w-5" />
-          </button>
-        )}
-      </div>
-      <div className="p-4 text-gray-500 dark:text-gray-400">
-        Time spent: {formatSeconds(task.time)}
-      </div>
-    </Card>
-  );
+    return (
+        <Card
+            className={`cursor-pointer hover:bg-gray-50 dark:hover:bg-zinc-700 transition-colors duration-500 ${className}`}
+        >
+            <div className="flex justify-between">
+                <div>
+                    <h3 className="text-lg font-medium">{task.title}</h3>
+                    <p className="text-gray-500 dark:text-gray-400">
+                        {task.description}
+                    </p>
+                    <div className="pt-4 text-gray-500 dark:text-gray-400">
+                        Time spent: {formatSeconds(task.time)}
+                    </div>
+                </div>
+
+                {onDeleteClick && (
+                    <button
+                        onClick={handleDeleteClick}
+                        className="p-2 text-gray-500 hover:text-red-500 transition-colors"
+                        aria-label="Delete task"
+                    >
+                        <Trash2 className="h-5 w-5" />
+                    </button>
+                )}
+            </div>
+        </Card>
+    );
 };
--- src/features/tasks-list/ui/DeleteTaskDialog.tsx
+++ src/features/tasks-list/ui/DeleteTaskDialog.tsx
@@ -1,40 +1,41 @@
 import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-} from "@/shared/ui";
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    buttonVariants,
+} from "@/shared/ui/neomorphic";
 
 interface Props {
-  isOpen: boolean;
-  onClose: () => void;
-  onConfirm: () => void;
+    isOpen: boolean;
+    onClose: () => void;
+    onConfirm: () => void;
 }
 
 export const DeleteTaskDialog = ({ isOpen, onClose, onConfirm }: Props) => {
-  return (
-    <AlertDialog open={isOpen} onOpenChange={onClose}>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>Are you sure?</AlertDialogTitle>
-          <AlertDialogDescription>
-            This action cannot be undone. Are you sure?
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction
-            onClick={onConfirm}
-            className="bg-red-500 hover:bg-red-600"
-          >
-            Delete
-          </AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
-  );
+    return (
+        <AlertDialog open={isOpen} onOpenChange={onClose}>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        This action cannot be undone. Are you sure?
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction
+                        onClick={onConfirm}
+                        className={buttonVariants({ variant: "destructive" })}
+                    >
+                        Delete
+                    </AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    );
 };
