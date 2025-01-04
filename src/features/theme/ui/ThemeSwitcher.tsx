64d53401181e31099cb636582d7f4fb142c30bc7
Viktor Schepliahin <silverman995@gmail.com>
2025-01-04 19:22:46 +0100

    feat(migrate to neumorpism): ThemeSwitcher component styles

--- src/features/theme/ui/ThemeSwitcher.tsx
+++ src/features/theme/ui/ThemeSwitcher.tsx
@@ -3,29 +3,30 @@ import { Moon, Sun } from "lucide-react";
 import { useThemeStore } from "../model/store";
 import { cn } from "@/shared/lib/utils";
 import { useEffect } from "react";
+import { Button } from "@/shared/ui/neomorphic";
 
 interface Props {
-  className?: string;
+    className?: string;
 }
 
 export const ThemeSwitcher = ({ className }: Props) => {
-  const { theme, setTheme } = useThemeStore();
+    const { theme, setTheme } = useThemeStore();
 
-  useEffect(() => {
-    setTheme(theme);
-  }, []);
+    useEffect(() => {
+        setTheme(theme);
+    }, []);
 
-  return (
-    <button
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-      className={cn(
-        "rounded-md p-2 hover:bg-gray-100 dark:hover:bg-gray-800 relative",
-        className,
-      )}
-    >
-      <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute top-2 left-2 h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-white" />
-      <span className="sr-only">Toggle theme</span>
-    </button>
-  );
+    return (
+        <Button
+            onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+            className={cn(
+                "w-10 h-10 rounded-full p-2 hover:bg-zinc-200 dark:hover:bg-zinc-700 relative",
+                className
+            )}
+        >
+            <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute top-3 left-3 h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-white" />
+            <span className="sr-only">Toggle theme</span>
+        </Button>
+    );
 };
