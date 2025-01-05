f5cca834d134857385443c74fbc3c6d90b30098c
Viktor Schepliahin <silverman995@gmail.com>
2025-01-05 20:22:47 +0100

    feat: play notification sound on session time gone

---
+++ src/features/notification/assets/index.ts
@@ -0,0 +1,5 @@
+export const FEATURE_ASSETS = {
+    sounds: {
+        notification: "/features/notification/assets/sounds/notification.mp3",
+    },
+} as const;
--- src/features/notification/lib/showNotification.ts
+++ src/features/notification/lib/showNotification.ts
@@ -1,5 +1,11 @@
+import { FEATURE_ASSETS } from "../assets";
+
 export function showNotification(title: string, options?: NotificationOptions) {
-  if (Notification.permission === "granted") {
-    new Notification(title, options);
-  }
+    if (Notification.permission === "granted") {
+        new Notification(title, options);
+        const audio = new Audio(FEATURE_ASSETS.sounds.notification);
+        audio.play().catch((error) => {
+            console.error("Failed to play notification sound:", error);
+        });
+    }
 }
