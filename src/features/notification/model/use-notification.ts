d7c0bc0cd7d364f54ed3a46076cd36a3aa843db0
Viktor Schepliahin <silverman995@gmail.com>
2025-01-05 20:23:47 +0100

    fix: fix false notifications firing on page loads

--- src/features/notification/model/use-notification.ts
+++ src/features/notification/model/use-notification.ts
@@ -4,22 +4,20 @@ import { requestNotificationPermission } from "../lib/requestNotificationPermiss
 import { showNotification } from "../lib/showNotification";
 
 interface NotificationConfig {
-  title: string;
-  options?: NotificationOptions;
+    title: string;
+    options?: NotificationOptions;
 }
 
 export const useTimerNotification = (config: NotificationConfig) => {
-  const mode = useTimerStore((state) => state.mode);
+    const timeLeft = useTimerStore((state) => state.timeLeft);
 
-  useEffect(() => {
-    requestNotificationPermission();
-  }, []);
+    useEffect(() => {
+        requestNotificationPermission();
+    }, []);
 
-  useEffect(() => {
-    if (mode === "break" || mode === "longBreak") {
-      showNotification(config.title, config.options);
-    } else if (mode === "work") {
-      showNotification(config.title, config.options);
-    }
-  }, [mode]);
+    useEffect(() => {
+        if (timeLeft === 0) {
+            showNotification(config.title, config.options);
+        }
+    }, [timeLeft]);
 };
