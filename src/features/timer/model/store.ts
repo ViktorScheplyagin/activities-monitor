fd451dc3d09d47d1371332bac4f9ea135296eb6b
Viktor Schepliahin <silverman995@gmail.com>
2025-01-05 20:23:16 +0100

    refactor: remove console logs

--- src/features/timer/model/store.ts
+++ src/features/timer/model/store.ts
@@ -3,61 +3,60 @@ import { TimerMode } from "./types";
 import { TIMER_OPTIONS } from "../constants/timerOptions";
 
 interface TimerState {
-  timeLeft: number;
-  mode: TimerMode;
-  isRunning: boolean;
-  workDuration: number;
-  breakDuration: number;
-  longBreakDuration: number;
-  sessionsCount: number;
-  setTimeLeft: (time: number) => void;
-  setMode: (mode: TimerMode) => void;
-  setIsRunning: (isRunning: boolean) => void;
-  setWorkDuration: (duration: number) => void;
-  setBreakDuration: (duration: number) => void;
-  setLongBreakDuration: (duration: number) => void;
-  incrementSession: () => void;
-  resetSession: () => void;
-  resetTimer: () => void;
+    timeLeft: number;
+    mode: TimerMode;
+    isRunning: boolean;
+    workDuration: number;
+    breakDuration: number;
+    longBreakDuration: number;
+    sessionsCount: number;
+    setTimeLeft: (time: number) => void;
+    setMode: (mode: TimerMode) => void;
+    setIsRunning: (isRunning: boolean) => void;
+    setWorkDuration: (duration: number) => void;
+    setBreakDuration: (duration: number) => void;
+    setLongBreakDuration: (duration: number) => void;
+    incrementSession: () => void;
+    resetSession: () => void;
+    resetTimer: () => void;
 }
 
 export const useTimerStore = create<TimerState>((set) => ({
-  mode: "work",
-  isRunning: false,
-  workDuration: TIMER_OPTIONS.work[0].value,
-  breakDuration: TIMER_OPTIONS.break[0].value,
-  longBreakDuration: TIMER_OPTIONS.longBreak[0].value,
-  timeLeft: TIMER_OPTIONS.work[0].value,
-  sessionsCount: 0,
-  setTimeLeft: (time) => set({ timeLeft: time }),
-  setMode: (mode) => set({ mode }),
-  setIsRunning: (isRunning) => set({ isRunning }),
-  setWorkDuration: (duration) => set({ workDuration: duration }),
-  setBreakDuration: (duration) => set({ breakDuration: duration }),
-  setLongBreakDuration: (duration) => set({ longBreakDuration: duration }),
-  incrementSession: () => {
-    console.log("incrementSession");
-    set((state) => ({ sessionsCount: state.sessionsCount + 1 }));
-  },
-  resetSession: () => set({ sessionsCount: 0 }),
-  resetTimer: () =>
-    set((state) => {
-      let timeLeft;
-      switch (state.mode) {
-        case "work":
-          timeLeft = state.workDuration;
-          break;
-        case "break":
-          timeLeft = state.breakDuration;
-          break;
-        case "longBreak":
-          timeLeft = state.longBreakDuration;
-          break;
-      }
-      return {
-        ...state,
-        timeLeft,
-        isRunning: false,
-      };
-    }),
+    mode: "work",
+    isRunning: false,
+    workDuration: TIMER_OPTIONS.work[0].value,
+    breakDuration: TIMER_OPTIONS.break[0].value,
+    longBreakDuration: TIMER_OPTIONS.longBreak[0].value,
+    timeLeft: TIMER_OPTIONS.work[0].value,
+    sessionsCount: 0,
+    setTimeLeft: (time) => set({ timeLeft: time }),
+    setMode: (mode) => set({ mode }),
+    setIsRunning: (isRunning) => set({ isRunning }),
+    setWorkDuration: (duration) => set({ workDuration: duration }),
+    setBreakDuration: (duration) => set({ breakDuration: duration }),
+    setLongBreakDuration: (duration) => set({ longBreakDuration: duration }),
+    incrementSession: () => {
+        set((state) => ({ sessionsCount: state.sessionsCount + 1 }));
+    },
+    resetSession: () => set({ sessionsCount: 0 }),
+    resetTimer: () =>
+        set((state) => {
+            let timeLeft;
+            switch (state.mode) {
+                case "work":
+                    timeLeft = state.workDuration;
+                    break;
+                case "break":
+                    timeLeft = state.breakDuration;
+                    break;
+                case "longBreak":
+                    timeLeft = state.longBreakDuration;
+                    break;
+            }
+            return {
+                ...state,
+                timeLeft,
+                isRunning: false,
+            };
+        }),
 }));
--- src/features/timer/model/timer.ts
+++ src/features/timer/model/timer.ts
@@ -2,58 +2,57 @@ import { useEffect } from "react";
 import { useTimerStore } from "./store";
 
 export const useTimerLogicSetup = () => {
-  const setTimeLeft = useTimerStore((state) => state.setTimeLeft);
-  const setMode = useTimerStore((state) => state.setMode);
-  const incrementSession = useTimerStore((state) => state.incrementSession);
-  const mode = useTimerStore((state) => state.mode);
-  const isRunning = useTimerStore((state) => state.isRunning);
-  const timeLeft = useTimerStore((state) => state.timeLeft);
-  const workDuration = useTimerStore((state) => state.workDuration);
-  const breakDuration = useTimerStore((state) => state.breakDuration);
-  const longBreakDuration = useTimerStore((state) => state.longBreakDuration);
-  const sessionsCount = useTimerStore((state) => state.sessionsCount);
+    const setTimeLeft = useTimerStore((state) => state.setTimeLeft);
+    const setMode = useTimerStore((state) => state.setMode);
+    const incrementSession = useTimerStore((state) => state.incrementSession);
+    const mode = useTimerStore((state) => state.mode);
+    const isRunning = useTimerStore((state) => state.isRunning);
+    const timeLeft = useTimerStore((state) => state.timeLeft);
+    const workDuration = useTimerStore((state) => state.workDuration);
+    const breakDuration = useTimerStore((state) => state.breakDuration);
+    const longBreakDuration = useTimerStore((state) => state.longBreakDuration);
+    const sessionsCount = useTimerStore((state) => state.sessionsCount);
 
-  const handleModeTransition = () => {
-    console.log("handleModeTransition", mode, sessionsCount);
-    switch (mode) {
-      case "work":
-        if (sessionsCount % 4 === 0 && sessionsCount > 0) {
-          setMode("longBreak");
-          setTimeLeft(longBreakDuration);
-        } else {
-          setMode("break");
-          setTimeLeft(breakDuration);
+    const handleModeTransition = () => {
+        switch (mode) {
+            case "work":
+                if (sessionsCount % 4 === 0 && sessionsCount > 0) {
+                    setMode("longBreak");
+                    setTimeLeft(longBreakDuration);
+                } else {
+                    setMode("break");
+                    setTimeLeft(breakDuration);
+                }
+                break;
+            case "break":
+            case "longBreak":
+                setMode("work");
+                setTimeLeft(workDuration);
+                break;
         }
-        break;
-      case "break":
-      case "longBreak":
-        setMode("work");
-        setTimeLeft(workDuration);
-        break;
-    }
-  };
-
-  useEffect(() => {
-    const timeWorker = new Worker(
-      new URL("../workers/timeWorker.ts", import.meta.url),
-    );
-
-    timeWorker.onmessage = () => {
-      setTimeLeft(timeLeft - 1);
     };
 
-    if (isRunning && timeLeft > 0) {
-      timeWorker.postMessage({ command: "start", interval: 1000 });
-    } else if (timeLeft === 0) {
-      handleModeTransition();
-      if (mode === "work") {
-        incrementSession();
-      }
-    }
+    useEffect(() => {
+        const timeWorker = new Worker(
+            new URL("../workers/timeWorker.ts", import.meta.url)
+        );
 
-    return () => {
-      timeWorker.postMessage({ command: "stop" });
-      timeWorker.terminate();
-    };
-  }, [isRunning, timeLeft, mode, workDuration, breakDuration]);
+        timeWorker.onmessage = () => {
+            setTimeLeft(timeLeft - 1);
+        };
+
+        if (isRunning && timeLeft > 0) {
+            timeWorker.postMessage({ command: "start", interval: 1000 });
+        } else if (timeLeft === 0) {
+            handleModeTransition();
+            if (mode === "work") {
+                incrementSession();
+            }
+        }
+
+        return () => {
+            timeWorker.postMessage({ command: "stop" });
+            timeWorker.terminate();
+        };
+    }, [isRunning, timeLeft, mode, workDuration, breakDuration]);
 };
