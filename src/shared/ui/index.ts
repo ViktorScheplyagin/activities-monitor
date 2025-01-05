705988e4ce2510100aa59239e585f76bb49349d8
Viktor Schepliahin <silverman995@gmail.com>
2025-01-05 11:59:16 +0100

    refactor: remove unnecessary UI components

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,12 +1,3 @@
-export { Button } from "./Button";
 export { Dialog } from "./dialog";
-export { Select } from "./Select";
-export { Input } from "./input";
 export { Label } from "./label";
-export { Textarea } from "./textarea";
-export { Popover } from "./popover";
-export { Checkbox } from "./checkbox";
-export * from "./Card";
 export * from "./form";
-export * from "./Select";
-export * from "./alert-dialog";
