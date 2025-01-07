513db2ea03764f539ed611c9b45abfeea46f1ec7
Viktor Schepliahin <silverman995@gmail.com>
2025-01-07 21:32:49 +0100

    feat: add hours count to the total time counter

--- src/entities/task/lib/formatSeconds.tsx
+++ src/entities/task/lib/formatSeconds.tsx
@@ -1,10 +1,13 @@
 import { TaskData } from "../api/dto/task";
 
 export function formatSeconds(time: TaskData["time"]) {
-  if (time) {
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
-    return `${minutes}:${seconds.toString().padStart(2, "0")}`;
-  }
-  return "00:00";
+    if (time) {
+        const totalMinutes = Math.floor(time / 60);
+        const hours = Math.floor(totalMinutes / 60);
+        const minutes = totalMinutes % 60;
+        const seconds = time % 60;
+
+        return `${hours}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+    }
+    return "00:00:00";
 }
