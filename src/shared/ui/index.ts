320c814cdeb87d896de3037953bf4c982d21b53f
Viktor Schepliahin <silverman995@gmail.com>
2025-02-28 13:47:49 +0100

    feat: export DropdownMenu, Popover, and Command components

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,3 +1,6 @@
 export { Dialog } from "./dialog";
 export { Label } from "./label";
 export * from "./form";
+export * from "./dropdown-menu";
+export * from "./popover";
+export * from "./command";
